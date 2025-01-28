c28b2a305a53fc11c4ca58848af0d28b6b92c386
parthokr <partho.rajvor2016@gmail.com>
2025-01-28 16:40:20 +0600

    test(auth): add test for normalized email

---
+++ backend/codesirius/authentication/tests/test_user_model.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = "test@example.com"
+        username = "testusername"
+        password = "testpassword"
+        user = get_user_model().objects.create_user(
+            email=email, username=username, password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertEqual(user.username, username)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        sample_emails = [
+            ("test1@EXAMPLE.com", "test1@example.com"),
+            ("Test2@Example.com", "Test2@example.com"),
+            ("TEST3@EXAMPLE.com", "TEST3@example.com"),
+            ("test4@example.COM", "test4@example.com"),
+        ]
+
+        for email, expected_email in sample_emails:
+            user = get_user_model().objects.create_user(
+                email, "testusername", "testpassword"
+            )
+            self.assertEqual(user.email, expected_email)
