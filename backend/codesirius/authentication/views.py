dbcf8a2b7cd267322c834f28af316bafc042584f
parthokr <partho.rajvor2016@gmail.com>
2025-01-31 02:36:50 +0600

    feat(token): add view to verify token

--- backend/codesirius/authentication/views.py
+++ backend/codesirius/authentication/views.py
@@ -1,44 +1,52 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.contrib.auth import authenticate
-from .serializers import SignupSerializer, SigninSerializer
 from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
+from yaml import serialize
+
+from authentication.serializers import (
+    SignupSerializer,
+    SigninSerializer,
+    TokenVerifySerializer,
+    CustomTokenVerifySerializer,
+)
+from codesirius.codesirius_api_response import CodesiriusAPIResponse
 
 User = get_user_model()
 
 
-def authenticate_by_email(email, password):
-    try:
-        user = get_user_model().objects.get(email=email)
-        if user.check_password(password):
-            return user
-    except get_user_model().DoesNotExist:
-        return None
-    return None
-
-
-class SignupView(APIView):
+class SignupAPIView(APIView):
     def post(self, request):
         serializer = SignupSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            return Response({"user_id": user.id}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class SigninView(APIView):
-    def post(self, request):
-        serializer = SigninSerializer(data=request.data)
-        if serializer.is_valid():
-            email = serializer.validated_data["email"]
-            password = serializer.validated_data["password"]
-
-            user = authenticate_by_email(email, password)
-            if user:
-                return Response({"user_id": user.id}, status=status.HTTP_200_OK)
-            return Response(
-                {"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
+            return CodesiriusAPIResponse(
+                data={"user_id": user.id},
+                status_code=status.HTTP_201_CREATED,
+                message="User created",
             )
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        raise ValidationError(serializer.errors)
+
+
+class SigninAPIView(TokenObtainPairView):
+    serializer_class = SigninSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return CodesiriusAPIResponse(
+            data=serializer.validated_data, message="Signin successful"
+        )
+
+
+class TokenVerifyAPIView(TokenVerifyView):
+    serializer_class = CustomTokenVerifySerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return CodesiriusAPIResponse(
+            data=serializer.validated_data, message="Token verified"
+        )
