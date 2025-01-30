f833a32d332c15048c1fbee890e8e31807c57aed
parthokr <partho.rajvor2016@gmail.com>
2025-01-31 02:36:06 +0600

    feat(route): add token/verify route

--- backend/codesirius/authentication/urls.py
+++ backend/codesirius/authentication/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import SignupView, SigninView
+
+from authentication.views import SignupAPIView, SigninAPIView, TokenVerifyAPIView
 
 urlpatterns = [
-    path("signup", SignupView.as_view(), name="signup"),
-    path("signin", SigninView.as_view(), name="signin"),
+    path("signup", SignupAPIView.as_view(), name="signup"),
+    path("signin", SigninAPIView.as_view(), name="signin"),
+    path("token/verify", TokenVerifyAPIView.as_view(), name="token"),
 ]
