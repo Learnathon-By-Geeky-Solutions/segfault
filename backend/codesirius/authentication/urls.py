13b1152d89f8d00bec07f4ee40cf1ad1f3d5e128
parthokr <partho.rajvor2016@gmail.com>
2025-01-31 15:51:55 +0600

    feat(whoami): add a new route to verify protected scope

--- backend/codesirius/authentication/urls.py
+++ backend/codesirius/authentication/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
-from authentication.views import SignupAPIView, SigninAPIView, TokenVerifyAPIView
+from authentication.views import (
+    SignupAPIView,
+    SigninAPIView,
+    TokenVerifyAPIView,
+    WhoAmIView,
+    TokenRefreshAPIView,
+)
 
 urlpatterns = [
     path("signup", SignupAPIView.as_view(), name="signup"),
     path("signin", SigninAPIView.as_view(), name="signin"),
-    path("token/verify", TokenVerifyAPIView.as_view(), name="token"),
+    path("token/verify", TokenVerifyAPIView.as_view(), name="token_verify"),
+    path("token/refresh", TokenRefreshAPIView.as_view(), name="token_refresh"),
+    path("whoami", WhoAmIView.as_view(), name="whoami"),
 ]
