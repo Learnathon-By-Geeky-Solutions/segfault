aef4949991ba497d2a5544dd757a8ea1f1b3de9a
parthokr <partho.rajvor2016@gmail.com>
2025-01-31 15:52:59 +0600

    feat(refresh): add TokenRefreshAPIView, WhoAmIView

--- backend/codesirius/authentication/views.py
+++ backend/codesirius/authentication/views.py
@@ -1,15 +1,19 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.views import APIView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
-from yaml import serialize
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenVerifyView,
+    TokenRefreshView,
+)
 
 from authentication.serializers import (
     SignupSerializer,
     SigninSerializer,
-    TokenVerifySerializer,
     CustomTokenVerifySerializer,
+    CustomTokenRefreshSerializer,
 )
 from codesirius.codesirius_api_response import CodesiriusAPIResponse
 
@@ -50,3 +54,39 @@ class TokenVerifyAPIView(TokenVerifyView):
         return CodesiriusAPIResponse(
             data=serializer.validated_data, message="Token verified"
         )
+
+
+class TokenRefreshAPIView(TokenRefreshView):
+    serializer_class = CustomTokenRefreshSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if serializer.validated_data["refresh"]:
+            return CodesiriusAPIResponse(
+                data=serializer.validated_data, message="Token refreshed"
+            )
+        raise ValidationError("Invalid refresh token")
+
+
+class WhoAmIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user_details = {
+            "id": request.user.id,
+            "username": request.user.username,
+            "email": request.user.email,
+            "is_staff": request.user.is_staff,
+            "is_superuser": request.user.is_superuser,
+            "is_active": request.user.is_active,
+            "created_at": request.user.created_at,
+            "updated_at": request.user.updated_at,
+            "created_by": (
+                request.user.created_by.id if request.user.created_by else None
+            ),
+            "updated_by": (
+                request.user.updated_by.id if request.user.updated_by else None
+            ),
+        }
+        return CodesiriusAPIResponse(data=user_details)
