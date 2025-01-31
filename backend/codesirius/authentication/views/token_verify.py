56f70e30cd0731e55edf7fcf7bacd2f83c7bd893
parthokr <partho.rajvor2016@gmail.com>
2025-02-01 02:32:46 +0600

    fix(response): remove data from response

--- backend/codesirius/authentication/views/token_verify.py
+++ backend/codesirius/authentication/views/token_verify.py
@@ -10,6 +10,4 @@ class TokenVerifyAPIView(TokenVerifyView):
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        return CodesiriusAPIResponse(
-            data=serializer.validated_data, message="Token verified"
-        )
+        return CodesiriusAPIResponse(message="Token verified")
