6a92de8837016deedda8813cf81f8e8274d72adc
parthokr <partho.rajvor2016@gmail.com>
2025-01-31 17:53:42 +0600

    feat(verification): generate code after creating user

--- backend/codesirius/authentication/serializers.py
+++ backend/codesirius/authentication/serializers.py
@@ -4,7 +4,10 @@ Serializers for authentication app.
 
 from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
-from rest_framework.exceptions import AuthenticationFailed, ValidationError
+from rest_framework.exceptions import (
+    AuthenticationFailed,
+    ValidationError,
+)
 from rest_framework.validators import UniqueValidator
 from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework_simplejwt.serializers import (
@@ -12,6 +15,8 @@ from rest_framework_simplejwt.serializers import (
     TokenVerifySerializer,
     TokenRefreshSerializer,
 )
+
+from authentication.models import VerificationCode
 
 User = get_user_model()
 
@@ -77,17 +82,26 @@ class SignupSerializer(serializers.Serializer):
 
     def validate(self, data):
         if data["password1"] != data["password2"]:
-            raise serializers.ValidationError("Passwords do not match")
+            raise serializers.ValidationError({"password2": ["Passwords do not match"]})
+        # Remove password2 from the validated data
+        data.pop("password2")
+        # Set the password field to password1
+        data["password"] = data.pop("password1")
+        # Return the validated data
         return data
 
     def create(self, validated_data):
+        """
+        Step 1: Create a new user with is_active=False
+        Step 2: Generate a verification code
+        Step 3: Send the verification code to the user
+        """
         user = User.objects.create_user(
-            first_name=validated_data["first_name"],
-            last_name=validated_data["last_name"],
-            email=validated_data["email"],
-            username=validated_data["username"],
-            password=validated_data["password1"],
-        )
+            is_active=False, **validated_data
+        )  # enforce is_active=False
+
+        verification_code = VerificationCode.objects.create(user=user)
+        verification_code.send_code()
         return user
 
 
@@ -112,32 +126,39 @@ class SigninSerializer(TokenObtainPairSerializer):
         if not user:
             raise AuthenticationFailed("Invalid credentials")
         if not user.is_active:
-            raise AuthenticationFailed("User is inactive")
-
+            raise AuthenticationFailed("Account is not active")
         return super().validate(attrs)
 
 
 class CustomTokenVerifySerializer(TokenVerifySerializer):
     token = serializers.CharField(
-        required=True, error_messages={"required": "Token is required"}
+        required=True,
+        error_messages={
+            "required": "Token is required",
+            "blank": "Token is required",
+        },
     )
 
     def validate(self, data):
         try:
             super().validate(data)
         except TokenError as e:
-            raise ValidationError(str(e))
+            raise ValidationError({"token": [str(e)]})
         return data
 
 
 class CustomTokenRefreshSerializer(TokenRefreshSerializer):
-    token = serializers.CharField(
-        required=True, error_messages={"required": "Token is required"}
+    refresh = serializers.CharField(
+        required=True,
+        error_messages={
+            "required": "Refresh token is required",
+            "blank": "Refresh token is required",
+        },
     )
 
     def validate(self, data):
         try:
             super().validate(data)
         except TokenError as e:
-            raise ValidationError(str(e))
+            raise ValidationError({"refresh": [str(e)]})
         return data
