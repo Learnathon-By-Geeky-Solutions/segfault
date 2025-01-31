a0219e1ecc8a1e5644a60e5aee919582786a5ea4
parthokr <partho.rajvor2016@gmail.com>
2025-01-31 17:53:17 +0600

    feat(verification): add VerificationCode model

--- backend/codesirius/authentication/models.py
+++ backend/codesirius/authentication/models.py
@@ -1,3 +1,7 @@
+import string
+from random import choices
+from datetime import datetime, timedelta
+
 from django.contrib.auth.models import (
     AbstractBaseUser,
     PermissionsMixin,
@@ -24,7 +28,7 @@ class UserManager(BaseUserManager):
             first_name=first_name,
             email=self.normalize_email(email),
             username=username,
-            **extra_fields
+            **extra_fields,
         )
 
         user.set_password(password)
@@ -53,3 +57,45 @@ class User(AbstractBaseUser, PermissionsMixin, BaseModel):
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["first_name", "email"]
+
+
+class VerificationCode(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    is_used = models.BooleanField(default=False)
+    used_at = models.DateTimeField(null=True, blank=True)
+    expires_at = models.DateTimeField()
+
+    @staticmethod
+    def generate_verification_code(length=6):
+        """Generate a random alphanumeric verification code."""
+        return "".join(choices(string.ascii_uppercase + string.digits, k=length))
+
+    def __str__(self):
+        return f"{self.user.username} - {self.code}"
+
+    def is_expired(self):
+        return self.expires_at < datetime.now()
+
+    def is_valid(self):
+        return not self.is_used and not self.is_expired()
+
+    def mark_as_used(self):
+        self.is_used = True
+        self.used_at = datetime.now()
+        self.save()
+
+    def save(self, *args, **kwargs):
+        if not self.code:
+            self.code = self.generate_verification_code()
+        if not self.expires_at:
+            self.expires_at = datetime.now() + timedelta(minutes=5)  # 5 minutes
+        super().save(*args, **kwargs)
+
+    def send_code(self):
+        pass
+
+    class Meta:
+        verbose_name = "Verification Code"
+        verbose_name_plural = "Verification Codes"
+        ordering = ["-created_at"]
