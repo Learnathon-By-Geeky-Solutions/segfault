6f4d83535b67a63ceeb75aba22184d2203a278fa
parthokr <partho.rajvor2016@gmail.com>
2025-02-01 16:13:14 +0600

    refactor(soc): separate out resend and check routes

--- backend/codesirius/authentication/urls.py
+++ backend/codesirius/authentication/urls.py
@@ -1,21 +1,31 @@
 from django.urls import path
 
 from authentication.views.auth import SignupAPIView
+from authentication.views.check_verification_code import CheckVerificationCodeAPIView
+from authentication.views.resend_verification_code import ResendVerificationCodeAPIView
 from authentication.views.token_obtain_pair import TokenObtainPairAPIView
 from authentication.views.token_refresh import TokenRefreshAPIView
 from authentication.views.token_verify import TokenVerifyAPIView
-from authentication.views.verification import VerificationCodeAPIView
 from authentication.views.whoami import WhoAmIView
 
 urlpatterns = [
-    path("signup", SignupAPIView.as_view(), name="signup"),
+    path("signup", SignupAPIView.as_view(), name="signup"),  # method: POST
     path(
-        "<int:user_id>/verification",
-        VerificationCodeAPIView.as_view(),
-        name="reset_verification_code",
-    ),
-    path("signin", TokenObtainPairAPIView.as_view(), name="signin"),
-    path("token/verify", TokenVerifyAPIView.as_view(), name="token_verify"),
-    path("token/refresh", TokenRefreshAPIView.as_view(), name="token_refresh"),
-    path("whoami", WhoAmIView.as_view(), name="whoami"),
+        "<int:user_id>/verification/resend",
+        ResendVerificationCodeAPIView.as_view(),
+        name="resend_verification_code",
+    ),  # method: PATCH
+    path(
+        "<int:user_id>/verification/check",
+        CheckVerificationCodeAPIView.as_view(),
+        name="check_verification_code",
+    ),  # method: POST
+    path("signin", TokenObtainPairAPIView.as_view(), name="signin"),  # method: POST
+    path(
+        "token/verify", TokenVerifyAPIView.as_view(), name="token_verify"
+    ),  # method: POST
+    path(
+        "token/refresh", TokenRefreshAPIView.as_view(), name="token_refresh"
+    ),  # method: POST
+    path("whoami", WhoAmIView.as_view(), name="whoami"),  # method: GET
 ]
