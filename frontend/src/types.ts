b49ecb5ff90b096ac1adcee62ced3e1d620f35c0
parthokr <partho.rajvor2016@gmail.com>
2025-02-02 04:46:20 +0600

    feat(codesiriusStateType): introduce codesiriusStateType for codesiriusStateContext

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,1 +1,8 @@
 export type themeType = 'light' | 'dark'
+
+export type codesiriusStateType = {
+    currentTheme: themeType,
+    setCurrentTheme: (theme: themeType) => void,
+    isCodesiriusLoading: boolean,
+    setCodesiriusLoading: (isLoading: boolean) => void
+}
