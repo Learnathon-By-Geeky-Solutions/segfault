d54f69513172fc205580f78783ab78174d6af968
parthokr <partho.rajvor2016@gmail.com>
2025-02-08 16:45:30 +0600

    feat(add-problem): ssr component to render add problem ui

---
+++ frontend/src/app/problems/add/page.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {redirect} from "next/navigation";
+import {headers} from "next/headers";
+import {User} from "@/lib/features/api/types";
+import AddProblem from "@/app/problems/add/add-problem";
+
+export const metadata = {
+    title: "Create a Problem",
+}
+
+const Page = async () => {
+    // const cookieStore = await cookies();
+    const headersList = await headers();
+
+    let user: User | null = null;
+    if (headersList.has('x-user')) {
+        const _user = headersList.get('x-user');
+        if (typeof _user === 'string') {
+            user = JSON.parse(_user);
+        }
+    }
+
+    if (!user) {
+        redirect("/auth/signin?next=/problems/add");
+    }
+
+    return (
+        <div>
+            <AddProblem />
+            {/*<SplitPane leftChildren={<h1>hi</h1>} rightChildren={<h1>hi</h1>}/>*/}
+        </div>
+    );
+};
+
+export default Page;
