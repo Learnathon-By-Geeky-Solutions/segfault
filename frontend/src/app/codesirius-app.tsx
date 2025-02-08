7b40b25a493490402cb5f3c34268bb7cb16562cf
parthokr <partho.rajvor2016@gmail.com>
2025-02-08 16:49:02 +0600

    fix(layout): remove grid

--- frontend/src/app/codesirius-app.tsx
+++ frontend/src/app/codesirius-app.tsx
@@ -2,7 +2,6 @@
 
 import {ReactNode} from 'react'
 import {ThemeProvider} from "@mui/material/styles";
-import {Grid} from "@mui/system";
 import CodesiriusAppBar from "@/components/codesirius-appbar";
 import {AppRouterCacheProvider} from "@mui/material-nextjs/v13-appRouter";
 import {useAppDispatch, useAppSelector} from "@/lib/hooks/hooks";
@@ -20,16 +19,14 @@ const CodesiriusApp = ({children, user}: CodesiriusThemeProviderProps) => {
     const theme = useAppSelector(state => state.codesirius.theme);
     const dispatch = useAppDispatch<AppDispatch>();
     const _user = useAppSelector(state => state.codesirius.user);
-    if (user && _user  === null) {
+    if (user && _user === null) {
         // only set user if it is not already set
         dispatch(setUser(user));
     }
     return (
         <AppRouterCacheProvider>
             <ThemeProvider theme={createCodeSiriusTheme(theme)}>
-                <Grid size={12}>
-                    <CodesiriusAppBar />
-                </Grid>
+                <CodesiriusAppBar/>
                 {children}
             </ThemeProvider>
         </AppRouterCacheProvider>
