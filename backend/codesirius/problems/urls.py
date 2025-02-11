6df5f0339a3cea0c65082e4aab30e21afdf1978e
parthokr <partho.rajvor2016@gmail.com>
2025-02-12 01:41:13 +0600

    feat(language): add routes for language

--- backend/codesirius/problems/urls.py
+++ backend/codesirius/problems/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from problems.views.language import (
+    LanguageListCreateAPIView,
+    LanguageRetrieveUpdateDestroyAPIView,
+)
 from problems.views.tag import TagListCreateAPIView, TagRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
@@ -8,5 +12,11 @@ urlpatterns = [
         "tags/<int:pk>",
         TagRetrieveUpdateDestroyAPIView.as_view(),
         name="tag-retrieve-update-destroy",
+    ),
+    path("languages", LanguageListCreateAPIView.as_view(), name="language-list-create"),
+    path(
+        "languages/<int:pk>",
+        LanguageRetrieveUpdateDestroyAPIView.as_view(),
+        name="language-retrieve-update-destroy",
     ),
 ]
