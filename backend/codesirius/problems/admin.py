b34c23cc924fdd6a2b3649c96522c1dc1f51d5ed
parthokr <partho.rajvor2016@gmail.com>
2025-02-11 23:52:43 +0600

    feat(register): register Language, Tag, Problem

--- backend/codesirius/problems/admin.py
+++ backend/codesirius/problems/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from problems.models import Language, Tag, Problem
 # Register your models here.
+
+admin.site.register(Language)
+admin.site.register(Tag)
+admin.site.register(Problem)
