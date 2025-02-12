0b07ea69fcff00ca836e945a34f0a29f8a236730
parthokr <partho.rajvor2016@gmail.com>
2025-02-13 02:36:08 +0600

    feat(models): add Solution, ReferenceSolution model

--- backend/codesirius/problems/models/__init__.py
+++ backend/codesirius/problems/models/__init__.py
@@ -1,5 +1,7 @@
 from problems.models.language import Language
-from problems.models.tag import Tag
 from problems.models.problem import Problem
+from problems.models.reference_solution import ReferenceSolution
+from problems.models.submission import Submission
+from problems.models.tag import Tag
 
-__all__ = ["Language", "Tag", "Problem"]
+__all__ = ["Language", "Tag", "Problem", "ReferenceSolution", "Submission"]
