0dcfd5523d825a5ac7a7ce45130e867488bfb739
parthokr <partho.rajvor2016@gmail.com>
2025-02-13 02:42:14 +0600

    feat(problems): add routes for problems

--- backend/codesirius/problems/urls.py
+++ backend/codesirius/problems/urls.py
@@ -4,9 +4,19 @@ from problems.views.language import (
     LanguageListCreateAPIView,
     LanguageRetrieveUpdateDestroyAPIView,
 )
+from problems.views.problem import (
+    ProblemListCreateAPIView,
+    ProblemRetrieveUpdateDestroyAPIView,
+)
 from problems.views.tag import TagListCreateAPIView, TagRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
+    path("", ProblemListCreateAPIView.as_view(), name="problem-list-create"),
+    path(
+        "<int:pk>",
+        ProblemRetrieveUpdateDestroyAPIView.as_view(),
+        name="problem-retrieve-update-destroy",
+    ),
     path("tags", TagListCreateAPIView.as_view(), name="tag-list-create"),
     path(
         "tags/<int:pk>",
