f5848988d365e440cbbbfb5af69bacaa8813160a
parthokr <partho.rajvor2016@gmail.com>
2025-02-14 16:41:44 +0600

    feat(LanguageAdmin): configure admin for Language

--- backend/codesirius/problems/admin.py
+++ backend/codesirius/problems/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django import forms
 
-from problems.models import Language, Tag, Problem
+from problems.models import Language, Tag, Problem, ReferenceSolution, Submission
 
 # Register your models here.
 
-admin.site.register(Language)
+
+class LanguageAdmin(admin.ModelAdmin):
+    exclude = ("created_by", "updated_by")
+
+    def save_model(self, request, obj, form, change):
+        if not obj.created_by:
+            obj.created_by = request.user
+        obj.updated_by = request.user
+
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Language, LanguageAdmin)
 admin.site.register(Tag)
 admin.site.register(Problem)
