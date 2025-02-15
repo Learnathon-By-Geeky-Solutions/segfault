def63f7068244d820db305cb22dad403a3b573a2
parthokr <partho.rajvor2016@gmail.com>
2025-02-16 01:07:57 +0600

    feat(problemsApiSlice): add problemsApiSlice slice

--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -1,15 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 import codesiriusReducer from './features/codesirius/codesiriusSlice'
 import {authApiSlice} from "@/lib/features/api/authApiSlice";
+import {problemsApiSlice} from "@/lib/features/api/problemsApiSlice";
 
 export const makeStore = () => {
     return configureStore({
         reducer: {
             codesirius: codesiriusReducer,
             [authApiSlice.reducerPath]: authApiSlice.reducer,
+            [problemsApiSlice.reducerPath]: problemsApiSlice.reducer
         },
         middleware: (getDefaultMiddleware) => {
-            return getDefaultMiddleware().concat(authApiSlice.middleware)
+            return getDefaultMiddleware().concat(
+                authApiSlice.middleware,
+                problemsApiSlice.middleware)
         }
     })
 }
