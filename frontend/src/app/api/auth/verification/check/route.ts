e5ce01436af92d2d2e5b856ba8cc7672c4fd10f8
parthokr <partho.rajvor2016@gmail.com>
2025-02-16 02:06:13 +0600

    fix(urls): load urls from env

--- frontend/src/app/api/auth/verification/check/route.ts
+++ frontend/src/app/api/auth/verification/check/route.ts
@@ -1,4 +1,6 @@
-import {DJANGO_BACKEND_URL} from "@/app/api/constants";
+require('dotenv').config();
+
+const DJANGO_BACKEND_URL = process.env.DJANGO_BACKEND_URL || "http://localhost:8000";
 
 export async function POST(req: Request) {
     const body = await req.json();
