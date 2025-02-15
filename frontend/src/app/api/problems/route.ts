da5d67008804f9816eda1386d00864476a65d70f
parthokr <partho.rajvor2016@gmail.com>
2025-02-16 01:09:35 +0600

    feat(api): add bff api routes for problems

---
+++ frontend/src/app/api/problems/route.ts
@@ -0,0 +1,30 @@
+import {DJANGO_BACKEND_URL} from "@/lib/constants";
+import {cookies} from "next/headers";
+
+export async function POST(req: Request) {
+    const cookieStore = await cookies();
+    const accessToken = cookieStore.get('access')?.value;
+    const body = await req.text();
+
+    const response = await fetch(`${DJANGO_BACKEND_URL}/api/v1/problems/`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${accessToken}`
+            },
+            body: body
+        });
+
+    // pass through the response from Django
+
+    return new Response(await response.text(), {
+        status: response.status,
+        statusText: response.statusText,
+        headers: {
+            'Content-Type': 'application/json',
+            'Set-Cookie': response.headers.get('Set-Cookie') ?? "",
+        }
+    });
+}
