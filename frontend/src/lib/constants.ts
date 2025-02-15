0b5bc1a309ed705b4fa2ceec66453abd68cae561
parthokr <partho.rajvor2016@gmail.com>
2025-02-15 18:43:23 +0600

    fix(BACKEND_URL): hardcode

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,3 +1,2 @@
-require('dotenv').config()
-export const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:8000'
+export const BACKEND_URL = 'http://74.235.140.127'
 export const DJANGO_BACKEND_URL = 'http://localhost:8000'
