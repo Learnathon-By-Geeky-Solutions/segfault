950453eb5ae2e56d5b5d628c063167e47cb85092
parthokr <partho.rajvor2016@gmail.com>
2025-02-15 18:40:20 +0600

    fix(BACKEND_URL): fix typo

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,3 +1,3 @@
 require('dotenv').config()
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000'
+export const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:8000'
 export const DJANGO_BACKEND_URL = 'http://localhost:8000'
