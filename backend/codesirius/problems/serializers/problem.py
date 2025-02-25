c793e5b0fd30b521fd9d0dbcc57f8c1974b6201d
parthokr <partho.kr@proton.me>
2025-02-25 23:36:47 +0600

    refactor(execution_constraints): manage execution_constraints through problem

--- backend/codesirius/problems/serializers/problem.py
+++ backend/codesirius/problems/serializers/problem.py
@@ -1,113 +1,194 @@
 import logging
-from typing import Dict
 
-from django.core.exceptions import ValidationError
 from rest_framework import serializers
-from rest_framework.exceptions import APIException
-from rest_framework.validators import UniqueValidator
 
-from problems.models import Problem, Tag, Language
+from problems.models import Problem, Language, Tag, ExecutionConstraint
+from problems.serializers.execution_constraint_v2 import (
+    ExecutionConstraintSerializerV2,
+)
+from problems.serializers.language import LanguageSerializer
+from problems.serializers.tag import TagSerializer
 
 logger = logging.getLogger(__name__)
 
 
-class ProblemSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(
-        max_length=200,
-        required=True,
-        error_messages={
-            "required": "The title field is required.",
-            "blank": "The title field must not be blank.",
-            "null": "The title field must not be null.",
-            "max_length": "The title field must not exceed 200 characters.",
-        },
-        validators=[
-            UniqueValidator(
-                queryset=Problem.objects.all(),
-                message="Problem with this title already exists.",
-            )
-        ],
-    )
-    description = serializers.CharField(
-        max_length=5000,
-        required=False,
-        error_messages={
-            "max_length": "The description field must not exceed 5000 characters.",
-            "null": "Either leave the description field blank \
-                or exclude it from the request.",
-        },
-    )
-    tags = serializers.PrimaryKeyRelatedField(
+class ProblemSerializer(serializers.ModelSerializer):
+    createdBy = serializers.PrimaryKeyRelatedField(read_only=True, source="created_by")
+
+    # this field is used to accept a list of language ids
+    languageIds = serializers.PrimaryKeyRelatedField(
         many=True,
-        queryset=Tag.objects.all(),
-        required=True,
-        error_messages={
-            "required": "Minimum one tag is required.",
-            "null": "Minimum one tag is required.",
-        },
-    )
-    languages = serializers.PrimaryKeyRelatedField(
-        many=True,
-        queryset=Language.objects.all(),
-        required=True,
-        error_messages={
-            "required": "Minimum one language is required.",
-            "null": "Minimum one language is required.",
-        },
-    )
-    status = serializers.ChoiceField(
-        choices=Problem.Status.choices, default=Problem.Status.DRAFT
+        queryset=Language.objects.only("id"),
+        write_only=True,
+        source="new_languages",
     )
 
-    def validate_tags(self, tags):
-        if not tags:
-            raise serializers.ValidationError("Minimum one tag is required.")
-        return tags
+    tagIds = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Tag.objects.only("id"), write_only=True, source="new_tags"
+    )
 
-    def validate_languages(self, languages):
-        if not languages:
-            raise serializers.ValidationError("Minimum one language is required.")
-        return languages
+    # this field is used to return a list of language objects
+    languages = LanguageSerializer(many=True, read_only=True)
 
-    def create(self, validated_data: Dict) -> Problem:
-        logger.info(f"Creating a new problem with data: {validated_data}")
-        try:
-            problem: Problem = Problem(
-                title=validated_data["title"], created_by=self.context["request"].user
+    # this field is used to return a list of tag objects
+    tags = TagSerializer(many=True, read_only=True)
+
+    executionConstraints = ExecutionConstraintSerializerV2(
+        many=True, source="execution_constraints", required=False
+    )
+
+    class Meta:
+        model = Problem
+        fields = [
+            "id",
+            "title",
+            "languageIds",
+            "languages",
+            "tagIds",
+            "tags",
+            "description",
+            "executionConstraints",
+            "status",
+            "createdBy",
+        ]
+        extra_kwargs = {
+            "status": {"default": Problem.Status.DRAFT, "read_only": True},
+            "id": {"read_only": True},
+        }
+
+    def validate_executionConstraints(self, execution_constraints):
+        """
+        Ensure that each execution constraint's languageId is present in language_ids.
+        """
+        # get the language ids from the initial data
+        language_ids = self.initial_data.get("languageIds")
+        if language_ids is None and self.instance:
+            language_ids = list(self.instance.languages.values_list("id", flat=True))
+            logger.info(f"language_ids: {language_ids}")
+
+        if not isinstance(language_ids, list):
+            raise serializers.ValidationError("language_ids must be a list.")
+
+        allowed_language_ids = set(map(int, language_ids))
+        logger.info(f"allowed_language_ids: {allowed_language_ids}")
+        execution_constraints_language_ids = [
+            constraint["language"].id for constraint in execution_constraints
+        ]
+        if not set(execution_constraints_language_ids).issubset(allowed_language_ids):
+            raise serializers.ValidationError(
+                "Invalid execution constraints:  "
+                "The language specified in each constraint must correspond to an "
+                "allowed language for the problem."
             )
-            problem.save()
-            problem.tags.set(validated_data["tags"])
-            problem.languages.set(validated_data["languages"])
-            logger.info("Problem created successfully")
-            return problem
-        except Exception as e:
-            logger.error(f"Error while creating problem - {e}")
-            raise APIException("Error while creating problem")
+        if len(execution_constraints) != len(set(execution_constraints_language_ids)):
+            raise serializers.ValidationError(
+                "Duplicate execution constraints for the same language are not allowed."
+                " Ensure each language has only one execution constraint defined."
+            )
+        return execution_constraints
+
+    @staticmethod
+    def _extract_related_data(validated_data):
+        """
+        Extract the related data from the validated data.
+        """
+        new_languages = validated_data.pop("new_languages", [])
+        new_tags = validated_data.pop("new_tags", [])
+        constraints_data = validated_data.pop("execution_constraints", [])
+        return new_languages, new_tags, constraints_data
+
+    def create(self, validated_data):
+        request_user = self.context["request"].user
+
+        # extract the related data
+        new_languages, new_tags, constraints_data = (
+            ProblemSerializer._extract_related_data(validated_data)
+        )
+
+        # create the problem instance without the languages, tags and constraints
+        problem = Problem.objects.create(created_by=request_user, **validated_data)
+        # now set the languages and tags
+        problem.languages.set(new_languages)
+        problem.tags.set(new_tags)
+
+        # create the execution constraints instances
+        new_constraints = [
+            ExecutionConstraint(problem=problem, **constraint_data)
+            for constraint_data in constraints_data
+        ]
+
+        # bulk create the constraints
+        if new_constraints:
+            ExecutionConstraint.objects.bulk_create(new_constraints)
+
+        return problem
 
     def update(self, instance, validated_data):
-        logger.info(f"Updating problem with ID: {instance.id}, Data: {validated_data}")
-        try:
-            instance.title = validated_data.get("title", instance.title)
-            instance.description = validated_data.get(
-                "description", instance.description
-            )
-            instance.tags.set(validated_data.get("tags", instance.tags.all()))
-            instance.languages.set(
-                validated_data.get("languages", instance.languages.all())
-            )
-            instance.status = validated_data.get("status", instance.status)
-            instance.save()
-            logger.info(f"Problem with ID: {instance.id} updated successfully")
-            return instance
-        except ValidationError as e:
-            """
-            handle ValidationError raised by clean() method in Problem model
-            """
-            logger.warning(
-                f"Validation error while updating problem with ID: {instance.id} {e}"
-            )
-            raise serializers.ValidationError(e.message_dict)
-        except Exception as e:
-            logger.error(f"Error updating problem with ID: {instance.id}: {e}")
-            raise APIException("Error while updating problem")
+        logger.info("Updating problem")
+        # Process language_ids and tag_ids only if they are provided in the validated data
+
+        # extract the related data
+        new_languages, new_tags, execution_constraints_data = (
+            ProblemSerializer._extract_related_data(validated_data)
+        )
+        if new_languages:
+            instance.languages.set(new_languages)
+            # remove the constraints for the languages that are not present in the new_languages
+            instance.execution_constraints.exclude(
+                language__id__in=[language.id for language in new_languages]
+            ).delete()
+
+        if new_tags:
+            instance.tags.set(new_tags)
+
+        # Update other fields, only if present in validated_data
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+
+        # Update Execution Constraints
+        if execution_constraints_data:
+            logger.info("Updating execution constraints")
+            existing_constraints = {
+                ec.language_id: ec for ec in instance.execution_constraints.all()
+            }
+            new_execution_constraints = []
+            updated_execution_constraints = []
+            updated_execution_constraints_language_ids = []
+
+            for constraint_data in execution_constraints_data:
+                language = constraint_data["language"]
+                if language.id in existing_constraints:
+                    # update the existing constraint
+                    constraint = existing_constraints[language.id]
+                    constraint.time_limit = constraint_data.get(
+                        "time_limit", constraint.time_limit
+                    )
+                    constraint.memory_limit = constraint_data.get(
+                        "memory_limit", constraint.memory_limit
+                    )
+                    updated_execution_constraints.append(constraint)
+                    updated_execution_constraints_language_ids.append(language.id)
+                else:
+                    new_execution_constraints.append(
+                        ExecutionConstraint(problem=instance, **constraint_data)
+                    )
+
+            # exclude the constraints that are not present in the updated data
+            instance.execution_constraints.exclude(
+                language__id__in=updated_execution_constraints_language_ids
+            ).delete()
+
+            # bulk update the constraints
+            if updated_execution_constraints:
+                ExecutionConstraint.objects.bulk_update(
+                    updated_execution_constraints,
+                    ["time_limit", "memory_limit"],
+                )
+
+            if new_execution_constraints:
+                logger.info("Creating new execution constraints")
+                ExecutionConstraint.objects.bulk_create(new_execution_constraints)
+
+        return instance
