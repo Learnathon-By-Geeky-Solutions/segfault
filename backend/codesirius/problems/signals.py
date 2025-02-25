1ebd8d630acb4c084070f8347fd14960965e59a0
takrim1999 <takrimulalom@gmail.com>
2025-02-25 23:37:13 +0600

    fix(ExecutionConstraint): singularize model name

--- backend/codesirius/problems/signals.py
+++ backend/codesirius/problems/signals.py
@@ -2,7 +2,7 @@
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 from problems.models.problem import Problem
-from problems.models.execution_constraints import ExecutionConstraints
+from problems.models.execution_constraint import ExecutionConstraint
 
 
 @receiver(m2m_changed, sender=Problem.languages.through)
@@ -10,10 +10,10 @@ def remove_execution_constraints_on_language_removal(
     sender, instance, action, pk_set, **kwargs
 ):
     """
-    Signal handler that deletes related ExecutionConstraints when a language
+    Signal handler that deletes related ExecutionConstraint when a language
     is removed from a Problem.
     """
     if action == "post_remove":
-        ExecutionConstraints.objects.filter(
+        ExecutionConstraint.objects.filter(
             problem=instance, language_id__in=pk_set
         ).delete()
