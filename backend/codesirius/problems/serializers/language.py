51c6be0f77d718a00d82a876eb81c2897a139da5
takrim1999 <takrimulalom@gmail.com>
2025-02-25 23:31:13 +0600

    fix(serializer): use ModelSerializer

--- backend/codesirius/problems/serializers/language.py
+++ backend/codesirius/problems/serializers/language.py
@@ -1,66 +1,10 @@
-import logging
-
-from django.db import IntegrityError
 from rest_framework import serializers
-from rest_framework.exceptions import APIException
 
 from problems.models import Language
 
-logger = logging.getLogger(__name__)
 
-
-class LanguageSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(
-        min_length=1,
-        max_length=50,
-        required=True,
-        error_messages={
-            "required": "The name field is required.",
-            "blank": "The name field must not be blank.",
-            "null": "The name field must not be null.",
-            "min_length": "The name field must be at least 1 character long.",
-            "max_length": "The name field must not exceed 50 characters.",
-        },
-    )
-    version = serializers.CharField(
-        min_length=1,
-        max_length=50,
-        required=True,
-        error_messages={
-            "required": "The version field is required.",
-            "blank": "The version field must not be blank.",
-            "null": "The version field must not be null.",
-            "max_length": "The version field must not exceed 50 characters.",
-            "min_length": "The version field must be at least 1 character long.",
-        },
-    )
-
-    def create(self, validated_data):
-        logger.info(f"Creating a new language with data: {validated_data}")
-        try:
-            language = Language.objects.create(**validated_data)
-            logger.info("Language created successfully")
-            return language
-        except IntegrityError:
-            logger.error("Language with the same name and version already exists")
-            raise serializers.ValidationError(
-                {
-                    "name": "Language with this name and the version already exists",
-                    "version": "Language with the name and this version already exists",
-                }
-            )
-        except Exception as e:
-            logger.error(f"Error while creating language - {e}")
-            raise APIException("Error while creating language")
-
-    def update(self, instance, validated_data):
-        logger.info(f"Updating language with ID: {instance.id}, Data: {validated_data}")
-        try:
-            instance.name = validated_data.get("name", instance.name)
-            instance.version = validated_data.get("version", instance.version)
-            instance.save()
-            logger.info(f"Language with ID: {instance.id} updated successfully")
-            return instance
-        except Exception as e:
-            logger.error(f"Error updating language with ID: {instance.id}: {e}")
+class LanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = ["id", "name", "version"]
+        read_only_fields = ["id"]
