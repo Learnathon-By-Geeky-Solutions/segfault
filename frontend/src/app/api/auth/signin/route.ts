6e019c623a3309f28afc15c6935670a3e13b7f68
parthokr <partho.kr@proton.me>
2025-04-25 01:17:35 +0600

    fix(cookies): conditionally enable domains

--- frontend/src/app/api/auth/signin/route.ts
+++ frontend/src/app/api/auth/signin/route.ts
@@ -1,70 +1,70 @@
-import {cookies} from "next/headers";
+import { cookies } from "next/headers";
 const DJANGO_BACKEND_URL = process.env.DJANGO_BACKEND_URL || "http://localhost:8000";
 
+const PRODUCTION = process.env.PRODUCTION === "true" || false;
+
 export async function POST(req: Request) {
-    try {
-        const drfResponse = await fetch(`${DJANGO_BACKEND_URL}/api/v1/auth/signin`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Cookie': req.headers.get('Cookie') ?? ""
-            },
-            body: JSON.stringify(await req.json())
-        });
+	try {
+		const drfResponse = await fetch(`${DJANGO_BACKEND_URL}/api/v1/auth/signin`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json',
+				'Cookie': req.headers.get('Cookie') ?? ""
+			},
+			body: JSON.stringify(await req.json())
+		});
 
-        const drfResponseJson = await drfResponse.json();
-        if (drfResponse.ok) {
-            // extract token from response
-            const {access, refresh} = drfResponseJson.data;
-            const cookieStore = await cookies();
-            cookieStore.set("access", access, {
-                path: "/",
-                sameSite: "none",
-                secure: true,
-                domain: ".codesirius.tech", // required to set cookie for subdomains (like sse)
-                httpOnly: true,
-            });
+		const drfResponseJson = await drfResponse.json();
+		if (drfResponse.ok) {
+			// extract token from response
+			const { access, refresh } = drfResponseJson.data;
+			const cookieStore = await cookies();
+			cookieStore.set("access", access, {
+				path: "/",
+				sameSite: "none",
+				secure: true,
+				...(PRODUCTION ? { domain: ".codesirius.tech" } : {}),
+			});
 
-            cookieStore.set("refresh", refresh, {
-                path: "/",
-                sameSite: "none",
-                secure: true,
-                domain: ".codesirius.tech",
-                httpOnly: true,
-            });
+			cookieStore.set("refresh", refresh, {
+				path: "/",
+				sameSite: "none",
+				secure: true,
+				...(PRODUCTION ? { domain: ".codesirius.tech" } : {}),
+			});
 
-            // use httpOnly: true, secure: true in production
+			// use httpOnly: true, secure: true in production
 
-            // remove token from response and add redirect url
-            delete drfResponseJson.data;
-            drfResponseJson["data"] = {redirect: "/"};
+			// remove token from response and add redirect url
+			delete drfResponseJson.data;
+			drfResponseJson["data"] = { redirect: "/" };
 
-            return new Response(JSON.stringify(drfResponseJson), {
-                status: drfResponse.status,
-                statusText: drfResponse.statusText,
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            });
-        }
-        // pass through error response
-        return new Response(JSON.stringify(drfResponseJson), {
-            status: drfResponse.status,
-            statusText: drfResponse.statusText,
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        });
-    } catch (e) {
-        console.error(e);
-        return new Response(JSON.stringify({message: "An error occurred"}), {
-            status: 500,
-            statusText: "Internal Server Error",
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        });
-    }
+			return new Response(JSON.stringify(drfResponseJson), {
+				status: drfResponse.status,
+				statusText: drfResponse.statusText,
+				headers: {
+					'Content-Type': 'application/json',
+				}
+			});
+		}
+		// pass through error response
+		return new Response(JSON.stringify(drfResponseJson), {
+			status: drfResponse.status,
+			statusText: drfResponse.statusText,
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		});
+	} catch (e) {
+		console.error(e);
+		return new Response(JSON.stringify({ message: "An error occurred" }), {
+			status: 500,
+			statusText: "Internal Server Error",
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		});
+	}
 }
 
