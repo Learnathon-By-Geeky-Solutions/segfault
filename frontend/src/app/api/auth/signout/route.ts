280688f4575a251346990765a9217603b4832b0f
parthokr <partho.kr@proton.me>
2025-04-25 02:20:07 +0600

    Fix cookies path

--- frontend/src/app/api/auth/signout/route.ts
+++ frontend/src/app/api/auth/signout/route.ts
@@ -1,16 +1,29 @@
-import {cookies} from "next/headers";
+import { cookies } from "next/headers";
 const NEXT_PUBLIC_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000";
 
+const PRODUCTION = process.env.PRODUCTION === "true" || false;
+
 export async function GET(req: Request) {
-    // signout
-    // delete access and refresh tokens
-    // redirect to /
-    if (!req.headers.get("x-user")) {
-        return Response.redirect(NEXT_PUBLIC_BACKEND_URL, 302);
-    }
-    const cookieStore = await cookies();
-    cookieStore.delete("access");
-    cookieStore.delete("refresh");
-    return Response.redirect(NEXT_PUBLIC_BACKEND_URL, 302);
+	// signout
+	// delete access and refresh tokens
+	// redirect to /
+	if (!req.headers.get("x-user")) {
+		return Response.redirect(NEXT_PUBLIC_BACKEND_URL, 302);
+	}
+	const cookieStore = await cookies();
+	console.log("Before delete:", cookieStore.getAll());
+	cookieStore.delete({
+		name: "access",
+		path: "/",
+		...(PRODUCTION ? { domain: ".codesirius.tech" } : {})
+	});
+
+	cookieStore.delete({
+		name: "refresh",
+		path: "/",
+		...(PRODUCTION ? { domain: ".codesirius.tech" } : {})
+	});
+	console.log("After delete:", cookieStore.getAll());
+	return Response.redirect(NEXT_PUBLIC_BACKEND_URL, 302);
 }
 
