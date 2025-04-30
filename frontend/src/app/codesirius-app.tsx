ec3d4e04e4a418b7e71601a1d7c83097b4eb2b9a
takrim1999 <takrimulalom@gmail.com>
2025-04-30 12:01:20 +0600

    Add a nice splash screen

--- frontend/src/app/codesirius-app.tsx
+++ frontend/src/app/codesirius-app.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {ReactNode} from 'react'
+import {ReactNode, useEffect, useState} from 'react'
 import {ThemeProvider} from "@mui/material/styles";
 import CodesiriusAppBar from "@/components/codesirius-appbar";
 import {AppRouterCacheProvider} from "@mui/material-nextjs/v13-appRouter";
@@ -10,6 +10,7 @@ import {setUser} from "@/lib/features/codesirius/codesiriusSlice";
 import {createCodeSiriusTheme} from "@/lib/utils/create-theme";
 import {User} from "@/lib/features/api/types";
 import { NotificationProvider } from '@/contexts/NotificationContext';
+import SplashScreen from '@/components/SplashScreen';
 
 interface CodesiriusThemeProviderProps {
     children: ReactNode;
@@ -20,14 +21,24 @@ const CodesiriusApp = ({children, user}: CodesiriusThemeProviderProps) => {
     const theme = useAppSelector(state => state.codesirius.theme);
     const dispatch = useAppDispatch<AppDispatch>();
     const _user = useAppSelector(state => state.codesirius.user);
+    const [isFirstRender, setIsFirstRender] = useState(true);
+
+    useEffect(() => {
+        if (isFirstRender) {
+            setIsFirstRender(false);
+        }
+    }, [isFirstRender]);
+
     if (user && _user === null) {
         // only set user if it is not already set
         dispatch(setUser(user));
     }
+
     return (
         <AppRouterCacheProvider>
             <ThemeProvider theme={createCodeSiriusTheme(theme)}>
                 <NotificationProvider>
+                    {isFirstRender && <SplashScreen />}
                     <CodesiriusAppBar/>
                     {children}
                 </NotificationProvider>
