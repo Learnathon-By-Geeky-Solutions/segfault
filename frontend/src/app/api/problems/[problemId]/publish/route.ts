6b3f898058053eaf0cab3e3d00985ee1a2314253
takrim1999 <takrimulalom@gmail.com>
2025-04-30 13:08:19 +0600

    Fix type for params

--- frontend/src/app/api/problems/[problemId]/publish/route.ts
+++ frontend/src/app/api/problems/[problemId]/publish/route.ts
@@ -3,12 +3,13 @@ const DJANGO_BACKEND_URL = process.env.DJANGO_BACKEND_URL || "http://localhost:8
 
 export async function POST(
     req: Request,
-    { params }: { params: { problemId: string } }
+    { params }: { params: Promise<{ problemId: number }> }
 ) {
     const cookieStore = await cookies();
     const accessToken = cookieStore.get('access')?.value;
+    const {problemId} = await params;
 
-    const response = await fetch(`${DJANGO_BACKEND_URL}/api/v1/problems/${params.problemId}/publish/`,
+    const response = await fetch(`${DJANGO_BACKEND_URL}/api/v1/problems/${problemId}/publish/`,
         {
             method: 'POST',
             headers: {
